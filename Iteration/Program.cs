861fa7a19f631be7827f34430bfff902f720872e
fulfordcs <fulfordcs@gmail.com>
2022-09-12 14:55:15 +0100

    Add project files.

---
+++ Iteration/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Repeat 100 times
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"I've eaten {i} pizza(s)");
+            }
+
+            // Repeat until the user says "Yes"
+            bool areWeNearlyThereYet = false;
+            while(areWeNearlyThereYet == false)
+            {
+                Console.WriteLine("Are we nearly there yet?");
+                areWeNearlyThereYet = Console.ReadLine().ToLower() == "yes";
+            }
+        }
+    }
+}
